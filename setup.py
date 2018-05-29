ce79e8f549e04fe5ea2cba8240a76ee5e2f7fd6e
Eric Robibaro <eric.robibaro@appdirect.com>
2018-05-29 12:18:18 -0400

    ReadMe added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NeutrinoDetector',
+    version='',
+    packages=[''],
+    url='',
+    license='Apache',
+    author='Eric Robibaro',
+    author_email='perlchild@robibaro.org',
+    description=''
+)
